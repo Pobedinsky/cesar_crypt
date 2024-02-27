c8b27e87d396cecc06d8fcc96a784d0a2896fc40
Max Pobedinsky <95541283+Pobedinsky@users.noreply.github.com>
2024-02-27 11:39:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def encode(text, k):
-  return ''.join(chr(ord(x) + k % 26) if x != ' ' else ' ' for x in text)
+  return ''.join(chr((ord(x) + k % 26)%26 + 65) if x != ' ' else ' ' for x in text.upper())
 
 def decode(text, k):
   return encode(text, -k)
